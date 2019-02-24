ce46b740b8f26a6f484999b08f7a28a265cbd6bd
Elkhan <elkhan.safarov10@yandex.ru>
2019-02-24 16:37:44 +0700

    PROJECT_HW_LESSON-5

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let open_btn = document.getElementById('open-btn'),
+	main_functions = document.querySelector('.main-functions'),
+	goods_item = document.querySelectorAll('.goods-item'),
+	button = document.getElementsByTagName('button'),
+	hire_employers_item = document.querySelectorAll('.hire-employers-item');
+
+console.log(open_btn);
+console.log(main_functions);
+console.log(goods_item);
+console.log(button);
+console.log(hire_employers_item);
